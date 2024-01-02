48c5c4642f18791ec17728df6e95e2e9c1e686cc
TrainingIT <43062220+TrainingITClass@users.noreply.github.com>
2024-01-02 18:45:47 +0100

    Add files via upload

---
+++ 7.3/src/main/java/com/sergiotrapiello/cursotesting/application/ui/Paths.java
@@ -0,0 +1,28 @@
+package com.sergiotrapiello.cursotesting.application.ui;
+
+/**
+ * Define paths de funcionalidades de la aplicación.
+ * 
+ * <p>
+ * Se organiza en subclases por bloques funcionales
+ * </p>
+ */
+public final class Paths {
+
+	private Paths() {
+		// private constructor to not allow instances
+	}
+
+	/**
+	 * Paths relativos a funcionalidades de gestión de Ticket
+	 */
+	public final class Ticket {
+		public static final String ISSUE = "ticket/issue";
+		public static final String CALCULATE_AMOUNT = "ticket/calculateAmount";
+
+		private Ticket() {
+			// private constructor to not allow instances
+		}
+	}
+
+}
